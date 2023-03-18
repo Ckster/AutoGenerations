6f2464ea462e890cbe45efb7a0bec97ec905a35d
Ckster <verleyeerick@gmail.com>
2023-03-18 13:37:09 -0600

    Adding commented prod database url

--- autoGenerations/database/config.py
+++ autoGenerations/database/config.py
@@ -1,8 +1,16 @@
 import os
+import json
 import configparser
 
 file = os.path.dirname(__file__)
+PROJECT_DIR = os.path.dirname(os.path.dirname(__file__))
+
 DATABASE_URL = f"sqlite:///{os.path.join(file, 'database.db')}"
+
+# with open(os.path.join(PROJECT_DIR, 'database_secrets.json'), 'r') as f:
+#     database_secrets = json.load(f)
+
+# DATABASE_URL = f"postgres://{database_secrets['username']}:{database_secrets['password']}@localhost:5432/autoGenerations";
 
 #c = configparser.ConfigParser()
 #c.read(os.path.join(os.path.dirname(file), 'credentials.ini'))
