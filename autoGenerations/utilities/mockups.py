6e01390e78f61e7a81a6e52befd3a932dafd999f
Ckster <verleyeerick@gmail.com>
2023-03-29 20:10:26 -0600

    Finishing mock generator and adding template mock images

--- autoGenerations/utilities/mockups.py
+++ autoGenerations/utilities/mockups.py
@@ -1,36 +1,44 @@
 import os.path
 
 from PIL import Image
+from tqdm.auto import tqdm
 
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
-def create_mockups(input_image_path: str, outdir: str):
+def create_mockups(input_image_path: str, out_dir: str) -> None:
+    """
+    Generates mock images for an input product image. Specify the output directory where you would like the files
+    written to. This directory will be made if it does not already exist.
+    Args:
+        input_image_path (str): Path to the product image to create mockups for
+        out_dir (str): Path to the directory where the output mock images will be written
+    """
     mockup_images = [
         # Blue wall
-        # {
-        #     '8x12': {
-        #         'position': (1087, 617),
-        #         'placeholder_dimensions': (322, 489),  # (width, height) in pixels
-        #         'path': os.path.join(PROJECT_DIR, 'data', 'mockup_images', 'blue_wall_8x12.png')
-        #     },
-        #     '16x24': {
-        #         'position': (923, 371),
-        #         'placeholder_dimensions': (650, 981),
-        #         'path': os.path.join(PROJECT_DIR, 'data', 'mockup_images', 'blue_wall_16x24.png')
-        #     },
-        #     '20x30': {
-        #         'position': (842, 248),
-        #         'placeholder_dimensions': (814, 1228),
-        #         'path': os.path.join(PROJECT_DIR, 'data', 'mockup_images', 'blue_wall_20x30.png')
-        #     },
-        #     '24x36': {
-        #         'position': (757, 122),
-        #         'placeholder_dimensions': (983, 1478),
-        #         'path': os.path.join(PROJECT_DIR, 'data', 'mockup_images', 'blue_wall_24x36.png')
-        #     }
-        # },
+        {
+            '8x12': {
+                'position': (1087, 617),
+                'placeholder_dimensions': (322, 489),  # (width, height) in pixels
+                'path': os.path.join(PROJECT_DIR, 'data', 'mockup_images', 'blue_wall_8x12.png')
+            },
+            '16x24': {
+                'position': (923, 371),
+                'placeholder_dimensions': (650, 981),
+                'path': os.path.join(PROJECT_DIR, 'data', 'mockup_images', 'blue_wall_16x24.png')
+            },
+            '20x30': {
+                'position': (842, 248),
+                'placeholder_dimensions': (814, 1228),
+                'path': os.path.join(PROJECT_DIR, 'data', 'mockup_images', 'blue_wall_20x30.png')
+            },
+            '24x36': {
+                'position': (757, 122),
+                'placeholder_dimensions': (983, 1478),
+                'path': os.path.join(PROJECT_DIR, 'data', 'mockup_images', 'blue_wall_24x36.png')
+            }
+        },
 
         # Bookshelf
         {
@@ -45,57 +53,65 @@ def create_mockups(input_image_path: str, outdir: str):
                 'path': os.path.join(PROJECT_DIR, 'data', 'mockup_images', 'bookshelf_16x24.png')
             },
             '20x30': {
-                'position': (954, 221),
-                'placeholder_dimensions': (701, 1059),
+                'position': (950, 223),
+                'placeholder_dimensions': (701, 1057),
                 'path': os.path.join(PROJECT_DIR, 'data', 'mockup_images', 'bookshelf_20x30.png')
             },
             '24x36': {
-                'position': (757, 122),
-                'placeholder_dimensions': (983, 1478),
+                'position': (879, 119),
+                'placeholder_dimensions': (840, 1266),
                 'path': os.path.join(PROJECT_DIR, 'data', 'mockup_images', 'bookshelf_24x36.png')
             }
         },
 
         # Table
-        # {
-        #     '8x12': {
-        #         'position': (1087, 617),
-        #         'placeholder_dimensions': (322, 489),  # (width, height) in pixels
-        #         'path': os.path.join(PROJECT_DIR, 'data', 'mockup_images', 'table_8x12.png')
-        #     },
-        #     '16x24': {
-        #         'position': (923, 371),
-        #         'placeholder_dimensions': (650, 981),
-        #         'path': os.path.join(PROJECT_DIR, 'data', 'mockup_images', 'table_16x24.png')
-        #     },
-        #     '20x30': {
-        #         'position': (842, 248),
-        #         'placeholder_dimensions': (814, 1228),
-        #         'path': os.path.join(PROJECT_DIR, 'data', 'mockup_images', 'table_20x30.png')
-        #     },
-        #     '24x36': {
-        #         'position': (757, 122),
-        #         'placeholder_dimensions': (983, 1478),
-        #         'path': os.path.join(PROJECT_DIR, 'data', 'mockup_images', 'table_24x36.png')
-        #     }
-        # }
+        {
+            '8x12': {
+                'position': (810, 681),
+                'placeholder_dimensions': (292, 438),  # (width, height) in pixels
+                'path': os.path.join(PROJECT_DIR, 'data', 'mockup_images', 'table_8x12.png')
+            },
+            '16x24': {
+                'position': (659, 458),
+                'placeholder_dimensions': (588, 889),
+                'path': os.path.join(PROJECT_DIR, 'data', 'mockup_images', 'table_16x24.png')
+            },
+            '20x30': {
+                'position': (586, 349),
+                'placeholder_dimensions': (737, 1105),
+                'path': os.path.join(PROJECT_DIR, 'data', 'mockup_images', 'table_20x30.png')
+            },
+            '24x36': {
+                'position': (513, 235),
+                'placeholder_dimensions': (884, 1331),
+                'path': os.path.join(PROJECT_DIR, 'data', 'mockup_images', 'table_24x36.png')
+            }
+        }
     ]
 
     dimensions = [
-    #    '8x12',
-    #    '16x24',
+        '8x12',
+        '16x24',
         '20x30',
-    #    '24x36'
+        '24x36'
     ]
+
+    os.makedirs(out_dir, exist_ok=True)
 
     product_image = Image.open(input_image_path)
 
-    for mockup_info in mockup_images:
-        for dimension in dimensions:
+    print(f'Generating {len(dimensions)} dimensions each for {len(mockup_images)} mockup images')
+
+    for mockup_info in tqdm(mockup_images):
+        for dimension in tqdm(dimensions):
             dim = mockup_info[dimension]['placeholder_dimensions']
             resized_image = product_image.resize(dim, Image.LANCZOS)
 
-            mockup_image = Image.open(mockup_info[dimension]['path'])
+            mock_image_path = mockup_info[dimension]['path']
+            mockup_image = Image.open(mock_image_path)
             top_left = (mockup_info[dimension]['position'][0], mockup_info[dimension]['position'][1])
             mockup_image.paste(resized_image, top_left, mask=resized_image)
-            mockup_image.show()
+
+            mockup_image.save(os.path.join(out_dir, os.path.basename(mock_image_path)))
+
+    print(f'Finished writing {len(mockup_images) * len(dimensions)} images to {out_dir}')
